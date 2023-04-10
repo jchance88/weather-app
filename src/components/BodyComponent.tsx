12d8d9732c7b12dde0c02215b38f611150844fda
James Chance <Jameswachance914@gail.com>
2023-04-10 14:39:04 -0400

    Update to weather page

--- src/components/BodyComponent.tsx
+++ src/components/BodyComponent.tsx
@@ -1,20 +1,73 @@
-import { Card, Grid } from "@mui/material";
+import { Card, Grid, Typography } from "@mui/material";
 import React from "react";
+import "../css/App.css";
 
-interface BodyComponentProps {
-  city: string;
-  country: string;
-}
+export const BodyComponent: React.FC = () => {
+  const weatherData = [
+    {
+      city: "Washington DC",
+      Temperature: "90 degrees",
+      FeelsLike: "100 degrees",
+      AirQuality: "Good",
+    },
+    {
+      city: "New York City",
+      Temperature: "50 degrees",
+      FeelsLike: "55 degrees",
+      AirQuality: "Good",
+    },
+    {
+      city: "Los Angeles",
+      Temperature: "100 degrees",
+      FeelsLike: "110 degrees",
+      AirQuality: "Bad",
+    },
+  ];
 
-export const BodyComponent: React.FC<BodyComponentProps> = ({
-  city,
-  country,
-}) => {
   return (
-    <Grid container paddingTop={2} justifyContent={"center"}>
-      <Grid item sm={10}>
-        <Card variant="elevation">TEST</Card>
-      </Grid>
-    </Grid>
+    <>
+      {weatherData.map((weather) => {
+        return (
+          <Grid container padding={2} justifyContent={"space-evenly"}>
+            <Grid item sm={5} style={{ padding: "15px" }}>
+              <Card variant="elevation">
+                <Typography style={{ padding: "10px" }}>City:</Typography>
+                <Typography style={{ padding: "10px" }}>
+                  {weather.city}
+                </Typography>
+              </Card>
+            </Grid>
+            <Grid item sm={5} style={{ padding: "15px" }}>
+              <Card variant="elevation">
+                <Typography style={{ padding: "10px" }}>
+                  Temperature:
+                </Typography>
+                <Typography style={{ padding: "10px" }}>
+                  {weather.Temperature}
+                </Typography>
+              </Card>
+            </Grid>
+            <Grid item sm={5} style={{ padding: "15px" }}>
+              <Card variant="elevation">
+                <Typography style={{ padding: "10px" }}>Feels Like:</Typography>
+                <Typography style={{ padding: "10px" }}>
+                  {weather.FeelsLike}
+                </Typography>
+              </Card>
+            </Grid>
+            <Grid item sm={5} style={{ padding: "15px" }}>
+              <Card variant="elevation">
+                <Typography style={{ padding: "10px" }}>
+                  Air Quality:
+                </Typography>
+                <Typography style={{ padding: "10px" }}>
+                  {weather.AirQuality}
+                </Typography>
+              </Card>
+            </Grid>
+          </Grid>
+        );
+      })}
+    </>
   );
 };
